973db8ee1554694b3f65f861722bae2a9f71ea0f
hyunjoong-getmiso <hyunjoong@getmiso.com>
2017-07-07 18:41:01 +0900

    graph on the way

--- response_time/views.py
+++ response_time/views.py
@@ -6,48 +6,9 @@ from long_convo import run_long
 from start_time import run_start
 from new_user import run_start
 
-# fusion charts api
-from fusioncharts import FusionCharts
 
 
 # Create your views here.
-
-column2d = FusionCharts("column2d", "ex1" , "600", "400", "chart-1", "json", 
-        """{  
-               "chart":
-               {  
-                  "caption":"Harry's SuperMart",
-                  "subCaption":"Top 5 stores in last month by revenue",
-                  "numberPrefix":"$",
-                  "theme":"ocean"
-               },
-               "data":
-               [  
-                  {  
-                     "label":"Bakersfield Central",
-                     "value":"880000"
-                  },
-                  {  
-                     "label":"Garden Groove harbour",
-                     "value":"730000"
-                  },
-                  {  
-                     "label":"Los Angeles Topanga",
-                     "value":"590000"
-                  },
-                  {  
-                     "label":"Compton-Rancho Dom",
-                     "value":"520000"
-                  },
-                  {  
-                     "label":"Daly City Serramonte",
-                     "value":"330000"
-                  }
-               ]
-        }""")
-
-
-
 
 
 def root(request):
@@ -101,10 +62,15 @@ def users(request):
 	if request.method == "POST":
 		crawl_size = int(request.POST.get("crawl_size"))
 		text = run_start(crawl_size)
-
+		new = {}
+		update = {}
+		for a in range(7):
+			new[a] = 1
+			update[a] =3
 		return render(request, "response_time/src/users.html",{
 				"text" : text,
-				"column2d" : column2d.render(),
+				"new" : new,
+				"update" : update,
 			})
 
 
